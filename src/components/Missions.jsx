f4b0ec64b5800b48b9987e7dc20ddd08a63695ea
Leonardo Leite <leoinatel@gmail.com>
2022-08-18 18:16:37 -0300

    Requisito 7.

---
+++ src/components/Missions.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        <h1>tag temporaria</h1>
+      </div>
+    );
+  }
+}
+
+export default Missions;
