f28603ca798956c38bd50f338ed3872b08013b7b
Leonardo Leite <leoinatel@gmail.com>
2022-08-18 16:12:02 -0300

    Requisito 2.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
     return (
       <main>
-        <p>Sistema Solar</p>
         <Header />
+        <SolarSystem />
       </main>
     );
   }
---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
