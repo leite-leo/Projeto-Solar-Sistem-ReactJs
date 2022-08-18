9ae80cd5d2f7e87afbc0a302eda4616f6a8a1de5
Leonardo Leite <leoinatel@gmail.com>
2022-08-18 18:19:48 -0300

    Requisito 8.

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Title from './Title';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
-        <h1>tag temporaria</h1>
+        <Title headline="Missões" />
       </div>
     );
   }
